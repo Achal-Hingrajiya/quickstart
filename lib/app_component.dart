6e6488750438b06a832eb9848f6b69b45cdf745e
Achal-H14 <apcomp1410@gmail.com>
2021-08-05 23:54:39 +0530

    created nav component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,10 +1,11 @@
 import 'package:angular/angular.dart';
 import 'package:angular_app/components/header/header_component.dart';
+import 'package:angular_app/components/navigation/nav_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [coreDirectives, HeaderComponent],
+  directives: [coreDirectives, HeaderComponent, NavComponent],
 )
 class AppComponent {
   var name = 'Angular';
