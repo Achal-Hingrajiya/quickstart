905d58ac521fc3455e5ebb74f7c1dec741687585
Achal-H14 <apcomp1410@gmail.com>
2021-08-05 23:44:04 +0530

    created header component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,10 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/components/header/header_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [coreDirectives],
+  directives: [coreDirectives, HeaderComponent],
 )
 class AppComponent {
   var name = 'Angular';
---
+++ lib/components/header/header_component.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-header',
+  templateUrl: 'header_component.html',
+  directives: [coreDirectives],
+)
+class HeaderComponent {}
